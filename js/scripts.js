830f55cd9837a825acb26481efca369daae7f312
Jason <jwright9441@gmail.com>
2019-05-08 15:27:33 -0400

    sorting alg

--- js/scripts.js
+++ js/scripts.js
@@ -179,7 +179,7 @@ $(function () {
                 var activityHTML = "";
 
 
-                JSONObject.sort(function(a, b){
+                JSONObject.sort(function(a, b) {
                     if(a.name < b.name) { return -1; }
                     if(a.name > b.name) { return 1; }
                     return 0;
